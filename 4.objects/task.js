9790f55b74c76d62460052840aeb8a5163d847d3
Pupipipu <zxkirillxz@gmail.com>
2025-12-01 20:56:48 +0700

    Четвертое задание готово

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,29 @@
 function Student(name, gender, age) {
-  
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
+  this.marks = [];
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  
+  this.subject = subjectName;
 }
 
 Student.prototype.addMarks = function (...marks) {
-  
+  if( "marks" in this) {
+    this.marks.push(...marks)
+  }
 }
 
 Student.prototype.getAverage = function () {
-  
+  if(!this.marks || this.marks.length === 0) {
+    return 0
+  }
+  return this.marks.reduce((acc, item) => acc + item, 0) / this.marks.length;
 }
 
 Student.prototype.exclude = function (reason) {
-  
+  delete this.subject;
+  delete this.marks;
+  this.excluded = reason;
 }
